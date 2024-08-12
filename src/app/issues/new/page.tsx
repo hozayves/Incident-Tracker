586673516020d1a9bfbb849c9b76387e8b95a29e
hozayves <muhozayves2014@gmail.com>
2024-08-12 15:25:34 +0200

    Extract ErrorMessage component

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchema";
 import { z } from "zod";
+import ErrorMessage from "@/components/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -34,13 +35,17 @@ export default function NewPage() {
                 })}
                 className=" space-y-3 flex flex-col">
                 <TextField.Root placeholder="Title" {...register('title')} />
-                {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+                <ErrorMessage>
+                    {errors.title?.message}
+                </ErrorMessage>
                 <Controller
                     name="description"
                     control={control}
                     render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
                 />
-                {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+                <ErrorMessage>
+                    {errors.description?.message}
+                </ErrorMessage>
                 <Button>Submit New Issues</Button>
             </form>
         </div>
---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+import { Text } from "@radix-ui/themes"
+import { PropsWithChildren, ReactNode } from "react"
+
+export default function ErrorMessage({ children }: PropsWithChildren) {
+    if (!children) return null
+    return (
+        <Text color="red" as="p">{children}</Text>
+    )
+}   
