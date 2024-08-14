060454cc84934b1d54bd828476d7a563dab80fad
hozayves <muhozayves2014@gmail.com>
2024-08-14 09:55:42 +0200

    Add edit button

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,9 +1,11 @@
 import { notFound } from "next/navigation"
 import prisma from "../../../../prisma/client"
-import { Card, Flex, Heading, Text } from "@radix-ui/themes"
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes"
 import IssueStatusBadge from "@/components/IssueStatusBadge"
 import Markdown from "react-markdown"
 import delay from "delay"
+import { Pencil2Icon } from "@radix-ui/react-icons"
+import Link from "next/link"
 
 export default async function IssueDetailPage({ params: { id } }: { params: { id: string } }) {
     const issue = await prisma.issue.findUnique({
@@ -16,15 +18,23 @@ export default async function IssueDetailPage({ params: { id } }: { params: { id
 
     await delay(2000)
     return (
-        <div>
-            <Heading>{issue.title}</Heading>
-            <Flex gap="3" align="center">
-                <IssueStatusBadge status={issue.status} />
-                <Text>{issue.createdAt.toDateString()}</Text>
-            </Flex>
-            <Card className="prose lg:prose-lg prose-zinc" mt="4">
-                <Markdown>{issue.description}</Markdown>
-            </Card>
-        </div>
+        <Grid columns={{ initial: "1", md: '2' }} gap="3">
+            <Box>
+                <Heading>{issue.title}</Heading>
+                <Flex gap="3" align="center">
+                    <IssueStatusBadge status={issue.status} />
+                    <Text>{issue.createdAt.toDateString()}</Text>
+                </Flex>
+                <Card className="prose lg:prose-lg prose-zinc" mt="4">
+                    <Markdown>{issue.description}</Markdown>
+                </Card>
+            </Box>
+            <Box>
+                <Button>
+                    <Pencil2Icon />
+                    <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+                </Button>
+            </Box>
+        </Grid>
     )
 }
