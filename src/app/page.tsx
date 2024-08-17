6706d6235acf673aa6f5ad1e649196d33faa9f22
hozayves <muhozayves2014@gmail.com>
2024-08-17 18:41:53 +0200

    Build issue Chart component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import LatestIssues from "./LatestIssues";
 import IssueSummary from "./IssueSummary";
 import prisma from "../../prisma/client";
+import IssueChart from "./IssueChart";
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: 'OPEN' } })
@@ -10,7 +11,8 @@ export default async function Home() {
   const closed = await prisma.issue.count({ where: { status: 'CLOSED' } })
   return (
     <>
-      <IssueSummary open={open} closed={closed} inProgress={inProgress} />
+      {/* <IssueSummary open={open} closed={closed} inProgress={inProgress} /> */}
+      <IssueChart open={open} closed={closed} inProgress={inProgress} />
     </>
   );
 }
